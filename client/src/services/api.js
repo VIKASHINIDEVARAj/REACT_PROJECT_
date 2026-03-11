8683d67e457bbc751d44c3c8ef5bc3367c02a497
VIKASHINIDEVARAj <vikashinidevaraj27@gmail.com>
2026-03-11 22:56:24 +0530

    fix

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -3,7 +3,6 @@ import axios from 'axios';
 const API = axios.create({
     baseURL:'https://react-project-libo.onrender.com/api', 
 });
-
 API.interceptors.request.use((req) => {
     const token = localStorage.getItem('token');
     if (token) {
