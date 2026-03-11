86d5404fa1a55ea6aa145b01c700870345dd0bfc
VIKASHINIDEVARAj <vikashinidevaraj27@gmail.com>
2026-03-11 22:32:28 +0530

    new bug

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
-import './index.css'; // Tailwind CSS inga thaan ulla varuthu
+import './index.css';  
 import { AuthProvider } from './context/AuthContext.jsx'; // Namma Water Tank
 
 ReactDOM.createRoot(document.getElementById('root')).render(
--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-    baseURL: 'https://react-project-libo.onrender.com/api   ', 
+    baseURL:'https://react-project-libo.onrender.com/api', 
 });
 
 API.interceptors.request.use((req) => {
