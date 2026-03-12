81c444a3157e536f0c5af8591154f1fbc3679990
VIKASHINIDEVARAj <vikashinidevaraj27@gmail.com>
2026-03-12 10:24:56 +0530

    new deploy

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,16 +1,17 @@
 import axios from 'axios';
 
 const API = axios.create({
-    baseURL:'https://react-project-libo.onrender.com/api', 
+    baseURL: import.meta.env.VITE_API_URL || '/api',
 });
-export default API;
+
 API.interceptors.request.use((req) => {
     const token = localStorage.getItem('token');
     if (token) {
-        // 'Bearer' ku apram oru space kandippa irukanum
-        req.headers.Authorization = `Bearer ${token}`; 
+        req.headers.Authorization = `Bearer ${token}`;
     }
     return req;
 }, (error) => {
     return Promise.reject(error);
 });
+
+export default API;
