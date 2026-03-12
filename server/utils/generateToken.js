ca2b10969e06136cda3c9b72afcc49ad8871c882
VIKASHINIDEVARAj <vikashinidevaraj27@gmail.com>
2026-03-12 09:43:48 +0530

    new deploy

--- server/utils/generateToken.js
+++ server/utils/generateToken.js
@@ -2,12 +2,8 @@ import jwt from 'jsonwebtoken';
 
 const generateToken = (id) => {
     return jwt.sign({ id }, process.env.JWT_SECRET, {
-        expiresIn: '30d', // Pass 30 days-ku valid-ah irukum
+        expiresIn: '30d',
     });
 };
 
 export default generateToken;
-// Server-la secret-a thedurom
-const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
-    expiresIn: '30d',
-});
